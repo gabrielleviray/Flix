71257416d85866e3c57e0157d5fcb6f803fd786d
Gabrielle Viray <gabrielleviray@Gabrielles-MacBook-Pro.local>
2020-09-10 16:25:34 -0700

    Added more features so users can view details of each movie. Implemented Tab Bar Navigation for 'Superhero' tab. In the 'Superhero' tab, users can view a grid layout of superhero movies, select a movie, view its details, and view its trailer linking to a youtube webpage.

---
+++ Flix/MovieTrailersViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieTrailersViewController.swift
+//  Flix
+//
+//  Created by Gabrielle Viray on 9/10/20.
+//  Copyright © 2020 gabrielleviray. All rights reserved.
+//
+
+import Foundation
